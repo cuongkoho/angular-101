c50a1dc8f7fc5f000b6fe4ed930ca8b3ba714f33
Cuong Tran <cuong@koho.ca>
2020-03-03 13:12:46 -0500

    Did the following things:
    * Generate project with 1 route
    * Generate 3 components: product-list, product-alerts, nav-bar
    * *ngFor, *ngIf, (click), [struturalDirective]
    * @Input - data binding
    * @Output - event emitter

---
+++ src/app/app.module.ts
@@ -0,0 +1,25 @@
+import { NgModule } from '@angular/core';
+import { BrowserModule } from '@angular/platform-browser';
+import { ReactiveFormsModule } from '@angular/forms';
+
+import { AppRoutingModule } from './app-routing.module';
+import { AppComponent } from './app.component';
+import { TopBarComponent } from './top-bar/top-bar.component';
+import { ProductListComponent } from './product-list/product-list.component';
+import { ProductAlertsComponent } from './product-alerts/product-alerts.component';
+
+@NgModule({
+  declarations: [
+    AppComponent,
+    TopBarComponent,
+    ProductListComponent,
+    ProductAlertsComponent,
+  ],
+  imports: [
+    BrowserModule,
+    AppRoutingModule,
+  ],
+  providers: [],
+  bootstrap: [AppComponent]
+})
+export class AppModule { }
